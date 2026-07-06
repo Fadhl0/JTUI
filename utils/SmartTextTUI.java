70b54c2ea60fbc2ab73dc71b04dece26954d378d
fadhl <fadhlhayderr@gmail.com>
2026-07-06 22:19:57 +0300

    adding table tag into SmartTextTUI

--- utils/SmartTextTUI.java
+++ utils/SmartTextTUI.java
@@ -12,40 +12,43 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-// <br>
-// <ul> and <li> with special attr `icon` at <ul>
-
 final public class SmartTextTUI extends TextTUI {
   private String xml;
 
   /**
- * Add XML tags to convert it to TextTUI.
- * <p>Supported elements:</p>
- * <ul>
- * <li><b>Supported tags:</b>
- * <ul>
- * <li>{@code <span>} - inline container used to mark up a part of a text.</li>
- * <li>{@code <p>} - automatically adds a single blank line after each element.</li>
- * <li>{@code <a>} - defines a hyperlink using the {@code href} attribute.</li>
- * <li>{@code <ul>} - unordered list container, supports custom {@code icon & gap} attributes.</li>
- * <ul>
- * <li>{@code icon}: Defines a custom bullet icon (defaults to a standard circle `•`).</li>
- * <li>{@code gap}: Specifies the left padding/spacing for the list item content. e.g., gap="2"</li>
- * </ul>
- * <li>{@code <li>} - unordered list item element.</li>
- * <li>{@code <br>} - inserts a single line break.</li>
- * </ul>
- * </li>
- * <li><b>Supported attributes across all tags:</b>
- * <ul>
- * <li>{@code color} supports hexadecimal color e.g., color="#00a919"</li>
- * <li>{@code bg-color} supports hexadecimal color  e.g., bg-color="#a90000"</li>
- * <li>{@code style} supports bold, italic, underline, strikethrough, hidden, and invert e.g., style="bold italic"</li>
- * </ul>
- * </li>
- * </ul>
- * <p><b>Note:</b> Tags must be wrapped in a single root element.</p>
- * @param xml the XML string to convert
+   * Add XML tags to convert it to TextTUI.
+   * <p>Supported elements:</p>
+   * <ul>
+   * <li><b>Supported tags:</b>
+   * <ul>
+   * <li>{@code <span>} - inline container used to mark up a part of a text.</li>
+   * <li>{@code <p>} - automatically adds a single blank line after each element.</li>
+   * <li>{@code <a>} - defines a hyperlink using the {@code href} attribute.</li>
+   * <li>{@code <ul>} - unordered list container, supports custom {@code icon & gap} attributes.</li>
+   * <ul>
+   * <li>{@code icon}: Defines a custom bullet icon (defaults to a standard circle `•`).</li>
+   * <li>{@code gap}: Specifies the left padding/spacing for the list item content. e.g., gap="2"</li>
+   * </ul>
+   * <li>{@code <li>} - unordered list item element.</li>
+   * <li>{@code <table>} - defines a structured grid table</li>
+   * <ul>
+   * <li>{@code gap}: Specifies the horizontal padding or spacing between columns (e.g., {@code gap="2"}).</li>
+   * </ul>
+   * <li>{@code <tr>} - defines a table row container.</li>
+   * <li>{@code <td>} - defines a table cell containing the column data.</li>
+   * <li>{@code <br>} - inserts a single line break.</li>
+   * </ul>
+   * </li>
+   * <li><b>Supported attributes across all tags:</b>
+   * <ul>
+   * <li>{@code color} supports hexadecimal color e.g., color="#00a919"</li>
+   * <li>{@code bg-color} supports hexadecimal color  e.g., bg-color="#a90000"</li>
+   * <li>{@code style} supports bold, italic, underline, strikethrough, hidden, and invert e.g., style="bold italic"</li>
+   * </ul>
+   * </li>
+   * </ul>
+   * <p><b>Note:</b> Tags must be wrapped in a single root element.</p>
+   * @param xml the XML string to convert
  */
   public SmartTextTUI(String xml) {
     xml = xml.replaceAll("(?i)<br\\s*/?>", "<p></p>");
@@ -82,6 +85,11 @@ final public class SmartTextTUI extends TextTUI {
 
   private Stack<String> parents = new Stack<>();
   private StringBuilder sb = new StringBuilder();
+
+  private List<TextTUI[]> table = new ArrayList<>();
+  private TextTUI[] tempRow = null;
+  private int indexRow = 0;
+
   // parse XML & apply changes
   private void parse(Element element, int currentDepth) {
 
@@ -89,6 +97,7 @@ final public class SmartTextTUI extends TextTUI {
     List<Integer> style = new ArrayList<>();
     String color = "";
     String bg = "";
+    int bufferTable = 3;
 
     for (int i = 0; i < attributes.getLength(); i++) {
       Node attr = attributes.item(i);
@@ -131,6 +140,23 @@ final public class SmartTextTUI extends TextTUI {
       
       sb.append("\n").append(" ".repeat(gap)).append(icon).append(" ");
     }
+    else if(tagName.equals("table")) {
+      if(element.hasAttribute("gap")) bufferTable = Integer.parseInt(element.getAttribute("gap"));
+      table.clear();
+    }
+    else if(tagName.equals("tr")) {
+      int tdCount = 0;
+      NodeList children = element.getChildNodes();
+      for (int i = 0; i < children.getLength(); i++) {
+        Node child = children.item(i);
+        if (child.getNodeType() == Node.ELEMENT_NODE && child.getNodeName().equalsIgnoreCase("td")) {
+          tdCount++;
+        }
+      }
+      tempRow = new TextTUI[tdCount]; 
+      indexRow = 0;
+    }
+
 
     NodeList children = element.getChildNodes();
     for (int i = 0; i < children.getLength(); i++) {
@@ -140,13 +166,35 @@ final public class SmartTextTUI extends TextTUI {
             parse((Element) child, currentDepth + 1);
         } else if (child.getNodeType() == Node.TEXT_NODE) {
             // text
-            sb.append(cleanText(child.getNodeValue()));
+            Node parentNode = child.getParentNode();
+            if (parentNode != null && parentNode.getNodeName().equalsIgnoreCase("td")) {
+              if (tempRow != null && indexRow < tempRow.length) {
+                tempRow[indexRow] = new TextTUI(child.getNodeValue()).setColor(color).setBackgroundColor(bg);
+                indexRow++;
+              }
+            } else {
+              sb.append(cleanText(child.getNodeValue()));
+            }
         }
     }
 
 
     if (tagName.equals("a")) {
       sb.append("\u001b]8;;\u001b\\");
+    }
+
+    if (tagName.equals("tr")) {
+      if (tempRow != null) {
+        table.add(tempRow);
+      }
+    }
+
+    if (tagName.equals("table")) {
+      TableTUI tablesTUI = new TableTUI().setBuffer(bufferTable);
+      for (TextTUI[] arr : table) {
+        tablesTUI.addRow(arr);
+      }
+      sb.append(tablesTUI.toString());
     }
 
 
